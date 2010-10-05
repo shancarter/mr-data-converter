e0520bd7522b8d0688c00cb1388f48288f3b86ba
Shan Carter <shan@Shan-Carter-Laptop.local>
2010-10-04 23:04:52 -0700

    Now autodetects number columns Added simple error reporting for parsing Slight wording modifications

--- js/converter.js
+++ js/converter.js
@@ -12,18 +12,18 @@ function DataConverter(nodeId) {
   //---------------------------------------
   
   this.nodeId             = nodeId;
-  this.node             = $("#"+nodeId);
+  this.node               = $("#"+nodeId);
   
   this.outputDataTypes        = [ 
-                                {"text":"Actionscript","id":"as"},
-                                {"text":"ASP/VBScript","id":"asp"},
-                                {"text":"HTML", "id":"html"},
-                                {"text":"JSON - Properties","id":"json"},
-                                {"text":"JSON - Arrays","id":"jsonArray"},
-                                {"text":"PHP","id":"php"},
-                                {"text":"Ruby","id":"ruby"},
-                                {"text":"XML - Properties","id":"xmlProperties"},
-                                {"text":"XML - Nodes","id":"xmlNodes"}];
+                                {"text":"Actionscript",       "id":"as",              "notes":""},
+                                {"text":"ASP/VBScript",       "id":"asp",             "notes":""},
+                                {"text":"HTML",               "id":"html",            "notes":""},
+                                {"text":"JSON - Properties",  "id":"json",            "notes":""},
+                                {"text":"JSON - Arrays",      "id":"jsonArray",       "notes":""},
+                                {"text":"PHP",                "id":"php",             "notes":""},
+                                {"text":"Ruby",               "id":"ruby",            "notes":""},
+                                {"text":"XML - Properties",   "id":"xmlProperties",   "notes":""},
+                                {"text":"XML - Nodes",        "id":"xmlNodes",        "notes":""}];
   this.outputDataType         = this.outputDataTypes[0]["id"];
   
   this.columnDelimiter        = "\t";
@@ -42,6 +42,10 @@ function DataConverter(nodeId) {
   this.newLine            = "\n";
   this.indent             = "  ";
   
+  this.errorLog           = "";
+  this.commentLine        = "//";
+  this.commentLineEnd        = "";
+  
   this.useUnderscores         = true;
   this.headersProvided        = true;
   this.downcaseHeaders        = true;
@@ -58,18 +62,16 @@ function DataConverter(nodeId) {
 DataConverter.prototype.create = function(w,h) {
   var self = this;
   
-  this.inputHeader = $('<div class="groupHeader" id="inputHeader"><p class="groupHeadline">Input CSV or tab-delimited data. <span class="subhead"> Using Excel? Simply copy and paste.<a href="#" id="insertSample">Use sample data</a></span></p></div>');
+  //build HTML for converter
+  this.inputHeader = $('<div class="groupHeader" id="inputHeader"><p class="groupHeadline">Input CSV or tab-delimited data. <span class="subhead"> Using Excel? Simply copy and paste. No data on hand? <a href="#" id="insertSample">Use sample</a></span></p></div>');
   this.inputTextArea = $('<textarea class="textInputs" id="dataInput"></textarea>');
   var outputHeaderText = '<div class="groupHeader" id="inputHeader"><p class="groupHeadline">Output as <select name="Data Types" id="dataSelector" >';
     for (var i=0; i < this.outputDataTypes.length; i++) {
       outputHeaderText += '<option value="'+this.outputDataTypes[i]["id"]+'">'+this.outputDataTypes[i]["text"]+'</option>';
     };
-    outputHeaderText += '</select><span class="subhead"><a href="#" id="convertButton">Convert</a></span></p></div>';
+    outputHeaderText += '</select><span class="subhead" id="outputNotes"></span></p></div>';
   this.outputHeader = $(outputHeaderText);
   this.outputTextArea = $('<textarea class="textInputs" id="dataOutput"></textarea>');
-  
-  
-  
   
   this.node.append(this.inputHeader);
   this.node.append(this.inputTextArea);
@@ -81,10 +83,10 @@ DataConverter.prototype.create = function(w,h) {
   
   //add event listeners
   
-  $("#convertButton").bind('click',function(evt){
-    evt.preventDefault();
-    self.convert();
-  });
+  // $("#convertButton").bind('click',function(evt){
+  //   evt.preventDefault();
+  //   self.convert();
+  // });
   
   $("#insertSample").bind('click',function(evt){
     evt.preventDefault();
@@ -109,25 +111,21 @@ DataConverter.prototype.resize = function(w,h) {
   var paneHeight = (h-90)/2;
   
   this.node.css({width:paneWidth});
-  
-  // this.inputHeader.css({width:paneWidth});
   this.inputTextArea.css({height:paneHeight});
-
-  // this.outputHeader.css({width:paneWidth});
   this.outputTextArea.css({height:paneHeight});  
   
 }
 
 DataConverter.prototype.convert = function() {
   
+  this.resetLog();
+
   this.inputText = this.inputTextArea.val();
   this.outputText = "";
   
   
   //make sure there is input data before converting...
   if (this.inputText.length > 0) {
-    
-    
     
     if (this.includeWhiteSpace === true) {
       this.newLine = "\n";
@@ -138,65 +136,98 @@ DataConverter.prototype.convert = function() {
     
     var dataArray = [];
     
+    //test for delimiter
+    //count the number of commas
     var RE = new RegExp("[^,]", "gi");
     var numCommas = this.inputText.replace(RE, "").length;
     
+    //count the number of tabs
     var RE = new RegExp("[^\t]", "gi");
     var numTabs = this.inputText.replace(RE, "").length;
     
+    //set delimiter
     if (numTabs > numCommas) {this.columnDelimiter = "\t"}
     else {this.columnDelimiter = ","}
     
-    var arr = this.inputText.split(this.rowDelimiter);
     
-    var numRows = 0;
-    var numColumns = 0;
+    var arr = this.inputText.split(this.rowDelimiter);
     
     for (var i=0; i < arr.length; i++) {
       dataArray.push(arr[i].split(this.columnDelimiter));
     };
     
+    
+    
     var headers = [];
     var headerTypes = [];
+    var headerNotes = [];
+    var numColumns = dataArray[0].length;
+    var numRows = dataArray.length;
     if (this.headersProvided) {
+      
+      //remove header row
       headers = dataArray.splice(0,1)[0];
+      numRows = dataArray.length;
+      
       for (var i=0; i < headers.length; i++) {
-        t = headers[i].split(":")
-        headers[i] = t[0]
+        
+        //remove and record header notes
+        t = headers[i].split(":");
+        headers[i] = t[0];
+        if (t[1]) {
+          headerNotes[i] = t[1].toLowerCase();
+        } else {
+          headerNotes[i] = "";
+        }
+        
+        //apply header transforms
         if (this.downcaseHeaders) {
           headers[i] = headers[i].toLowerCase();
         } 
         if (this.upcaseHeaders) {
           headers[i] = headers[i].toUpperCase();
         }
-        
         if (this.useUnderscores) {
           headers[i] = headers[i].split(' ').join('_');
         };
         
+        //remove unwanted characters from headers
         headers[i] = headers[i].split('-').join('_');
-        if (t[1]) {
-          headerTypes[i] = t[1].toLowerCase();
-        } else {
-          headerTypes[i] = "";
-        }
-        
       };
-      numColumns = headers.length;
-      numRows = dataArray.length;
-    } else {
-      numColumns = dataArray[0].length
-      numRows = dataArray.length;
+      
+    } else { //if no headers provided
+      
+      //create generic property names
       for (var i=0; i < numColumns; i++) {
-        headers.push("val"+String(i+1));
+        headers.push("val"+String(i));
         headerTypes.push("");
+        headerNotes.push("");
       };
     
     }
     
+    //test all the rows for proper number of columns.
+    for (var i=0; i < dataArray.length; i++) {
+      var numValues = dataArray[i].length;
+      if (numValues != numColumns) {this.log("Error parsing row "+String(i)+". Wrong number of columns.")};
+    };
     
+    //test columns for number data type
+    var numRowsToTest = Math.min(4,numColumns);
+    for (var i=0; i < headers.length; i++) {
+      var n = true;
+      for (var r=0; r < numRowsToTest; r++) {
+        if (!this.isNumber(dataArray[r][i])) { n = false };
+      };
+      if (n) {headerTypes[i] = "number"};
+    }
+    
+    
+    //Begin Rendering
     //ACTIONSCRIPT
     if (this.outputDataType === "as") {
+      this.commentLine = "//";
+      this.commentLineEnd = "";
       this.outputText += "[";
       for (var i=0; i < numRows; i++) {
         var row = dataArray[i];
@@ -217,6 +248,8 @@ DataConverter.prototype.convert = function() {
     
     //ASP
     } else if (this.outputDataType === "asp") {
+      this.commentLine = "'";
+      this.commentLineEnd = "";
       for (var i=0; i < numRows; i++) {
         var row = dataArray[i];
         for (var j=0; j < numColumns; j++) {
@@ -232,6 +265,8 @@ DataConverter.prototype.convert = function() {
     
     //HTML
     } else if (this.outputDataType === "html"){
+      this.commentLine = "<!--";
+      this.commentLineEnd = "-->";
       this.outputText += "<table>"+this.newLine;
       this.outputText += this.indent+"<thead>"+this.newLine;
       this.outputText += this.indent+this.indent+"<tr>"+this.newLine;
@@ -265,6 +300,8 @@ DataConverter.prototype.convert = function() {
     
     //RUBY
     } else if (this.outputDataType === "ruby") {
+      this.commentLine = "#";
+      this.commentLineEnd = "";
       this.outputText += "[";
       for (var i=0; i < numRows; i++) {
         var row = dataArray[i];
@@ -285,6 +322,8 @@ DataConverter.prototype.convert = function() {
     
       //PHP
     } else if (this.outputDataType === "php") {
+      this.commentLine = "//";
+      this.commentLineEnd = "";
       this.outputText += "array(" + this.newLine;
       for (var i=0; i < numRows; i++) {
         var row = dataArray[i];
@@ -305,6 +344,8 @@ DataConverter.prototype.convert = function() {
     
       //JSON - properties
     } else if (this.outputDataType === "json") {
+      this.commentLine = "//";
+      this.commentLineEnd = "";
       this.outputText += "[";
       for (var i=0; i < numRows; i++) {
         var row = dataArray[i];
@@ -328,6 +369,8 @@ DataConverter.prototype.convert = function() {
     
         //JSON - array
       } else if (this.outputDataType === "jsonArray") {
+        this.commentLine = "//";
+        this.commentLineEnd = "";
         this.outputText += "[";
         for (var i=0; i < numColumns; i++) {
           this.outputText += "[";
@@ -349,6 +392,10 @@ DataConverter.prototype.convert = function() {
     
       //XML - nodes
     } else if (this.outputDataType === "xmlNodes"){
+      
+      
+      this.commentLine = "<!--";
+      this.commentLineEnd = "-->";
       this.outputText = '<?xml version="1.0" encoding="UTF-8"?>' + this.newLine;
       this.outputText += "<rows>"+this.newLine;
       for (var i=0; i < numRows; i++) {
@@ -367,6 +414,9 @@ DataConverter.prototype.convert = function() {
     
       //XML properties
     } else if (this.outputDataType === "xmlProperties"){
+      
+      this.commentLine = "<!--";
+      this.commentLineEnd = "-->";
       this.outputText = '<?xml version="1.0" encoding="UTF-8"?>' + this.newLine;
       this.outputText += "<rows>"+this.newLine;
       for (var i=0; i < numRows; i++) {
@@ -381,14 +431,42 @@ DataConverter.prototype.convert = function() {
       this.outputText += "</rows>";
     }
     
-    
-    
-    this.outputTextArea.val(this.outputText);
+    this.outputTextArea.val(this.getLog()+this.outputText);
     
   }; //end test for existence of input text
 }
 
 
 DataConverter.prototype.insertSampleData = function() {
-  this.inputTextArea.val("NAME\tVALUE:number\tCOLOR\nAlan\t12\tblue\nShan\t13\tgreen\nJoe\t45\torange");
+  this.inputTextArea.val("NAME\tVALUE\tCOLOR\nAlan\t12\tblue\nShan\t13\tgreen\nJoe\t45\torange");
+}
+
+DataConverter.prototype.resetLog = function() {
+  this.errorLog = "";
+}
+
+DataConverter.prototype.log = function(l) {
+  this.errorLog += (String(l) + "<EOL>" );
+}
+
+DataConverter.prototype.getLog = function(l) {
+  if (this.errorLog.length > 0) {
+    var arr = this.errorLog.split("<EOL>");
+    var out = "";
+    for (var i=0; i < arr.length-1; i++) {
+      out += (this.commentLine + arr[i] + this.commentLineEnd+"\n");
+    };
+    return (this.commentLine + "ERRORS"+this.commentLineEnd+"\n" + out + "\n")
+  } else {
+    return ""
+  }
+  this.errorLog += ( this.commentLine+String(l) + "\n" );
+}
+
+
+DataConverter.prototype.isNumber = function(string) {
+  if( (string == null) || isNaN( new Number(string) ) ) {
+    return false;
+  }
+  return true;
 }
