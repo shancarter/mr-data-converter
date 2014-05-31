8f3f7bcaff1b911bb40b8b764b1a214430b6a276
David Young <david@deepinthecode.com>
2014-05-31 18:24:13 -0500

    Create getUrlVars.js

---
+++ js/getUrlVars.js
@@ -0,0 +1,14 @@
+// Read a page's GET URL variables and return them as an associative array.
+// from http://stackoverflow.com/questions/4656843/jquery-get-querystring-from-url
+function getUrlVars()
+{
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
