e8adefe5adb1aeb8c5462efadcfeaae5fe1ddaa7
David Young <david@deepinthecode.com>
2014-06-01 11:05:38 -0500

    Update addTextInputs.js
    Fixed widths of boxes

--- js/addTextInputs.js
+++ js/addTextInputs.js
@@ -6,7 +6,7 @@ function addTextInputs(targets){
   
   $(InputsWrapper).html("");
   for (i = 0; i < targets.length; i++) {
-    $(InputsWrapper).append('<input type="text" id="target_' + i.toString() + '" value="' + targets[i] + '" /><select id="mapping_' + i.toString() + '"></select>');
+    $(InputsWrapper).append('<input style="width: 50%" type="text" id="target_' + i.toString() + '" value="' + targets[i] + '" /><select style="width: 50%" id="mapping_' + i.toString() + '"></select>');
   };
   return false;
   
