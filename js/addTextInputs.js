e0ee9d2cdf4dd2771576e0e79af35c48bab4c044
David Young <david@deepinthecode.com>
2014-06-01 11:30:47 -0500

    Update addTextInputs.js

--- js/addTextInputs.js
+++ js/addTextInputs.js
@@ -8,10 +8,17 @@ function addTextInputs(targets,headerNames){
         headerOptions += "  <option>" + headerNames[j].toString() + "</option>\r\n";
     }
     for (i = 0; i < targets.length; i++) {
+      var strInputs = 'Target ' + (i+1).toString() + ':<input style="width: 50%" type="text" id="target_' + i.toString() + '" value="' + targets[i] + '" readonly /><br />' 
+        + 'Mapping ' + (i+1).toString() + ':<select style="width: 50%" id="mapping_' + i.toString() + '">'
+        + headerOptions
+        + '</select><br /><br />';
+      $(InputsWrapper).append(strInputs);
+      /*
       $(InputsWrapper).append('Target ' + (i+1).toString() + ':<input style="width: 50%" type="text" id="target_' + i.toString() + '" value="' + targets[i] + '" readonly /><br />');
       $(InputsWrapper).append('Mapping ' + (i+1).toString() + ':<select style="width: 50%" id="mapping_' + i.toString() + '">');
       $(InputsWrapper).append(headerOptions);
       $(InputsWrapper).append('</select><br /><br />');
+      */
     };
     return false;
   });
