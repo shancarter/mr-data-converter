35243cc4b8697ddc3eb49cec1ad62c534dc4ca33
David Young <david@deepinthecode.com>
2014-05-31 22:13:20 -0500

    Create jsonToCsv.js

---
+++ js/jsonToCsv.js
@@ -0,0 +1,19 @@
+// JSON to CSV Converter
+// taken from http://stackoverflow.com/questions/8847766/how-to-convert-json-to-csv-format-and-store-in-a-variable
+function ConvertToCSV(objArray) {
+    var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+    var str = '';
+
+    for (var i = 0; i < array.length; i++) {
+        var line = '';
+        for (var index in array[i]) {
+            if (line != '') line += ','
+
+            line += array[i][index];
+        }
+
+        str += line + '\r\n';
+    }
+
+    return str;
+}
