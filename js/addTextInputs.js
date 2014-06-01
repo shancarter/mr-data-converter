59339f570e9816f7ac458a9ccd4035f772f99c0e
David Young <david@deepinthecode.com>
2014-06-01 11:24:19 -0500

    Update addTextInputs.js
    add Options

--- js/addTextInputs.js
+++ js/addTextInputs.js
@@ -1,11 +1,16 @@
-function addTextInputs(targets){
+function addTextInputs(targets,headerNames){
   $(document).ready(function() {
     var InputsWrapper   = $("#InputsWrapper"); //Input boxes wrapper ID
     
     $(InputsWrapper).html("");
+    var headerOptions = "";
+    for (j = 0; j < headerNames.length; j++) {
+        headerOptions += "  <option>" + headerNames[j].toString() + "</option>\r\n";
+    }
     for (i = 0; i < targets.length; i++) {
       $(InputsWrapper).append('Target ' + (i+1).toString() + ':<input style="width: 50%" type="text" id="target_' + i.toString() + '" value="' + targets[i] + '" readonly /><br />');
       $(InputsWrapper).append('Mapping ' + (i+1).toString() + ':<select style="width: 50%" id="mapping_' + i.toString() + '">');
+      $(InputsWrapper).append(headerOptions);
       $(InputsWrapper).append('</select><br /><br />');
     };
     return false;
