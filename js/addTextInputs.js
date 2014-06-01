8210e50531f9d41eac4afbf998bcfd169815ce31
David Young <david@deepinthecode.com>
2014-06-01 10:31:36 -0500

    Create addTextInputs.js

---
+++ js/addTextInputs.js
@@ -0,0 +1,40 @@
+function addTextInputs(targets){
+  $(document).ready(function() {
+  
+  //var MaxInputs       = 8; //maximum input boxes allowed
+  var InputsWrapper   = $("#InputsWrapper"); //Input boxes wrapper ID
+  
+  for (i = 0; i < targets.length; i++) {
+    $(InputsWrapper).append('<input type="text" id="target_' + i.toString() + '" value="' + targets[i] + '" /><input type="text" id="mapping_' + i.toString() + '" />');
+  };
+  return false;
+  
+  /*
+  var AddButton       = $("#AddMoreFileBox"); //Add button ID
+  
+  var x = InputsWrapper.length; //initlal text box count
+  var FieldCount=1; //to keep track of text box added
+  
+  $(AddButton).click(function (e)  //on add input button click
+  {
+          if(x <= MaxInputs) //max input box allowed
+          {
+              FieldCount++; //text box added increment
+              //add input box
+              $(InputsWrapper).append('<div><input type="text" name="mytext[]" id="field_'+ FieldCount +'" value="Text '+ FieldCount +'"/><a href="#" class="removeclass">&times;</a></div>');
+              x++; //text box increment
+          }
+  return false;
+  });
+  
+  $("body").on("click",".removeclass", function(e){ //user click on remove text
+          if( x > 1 ) {
+                  $(this).parent('div').remove(); //remove text box
+                  x--; //decrement textbox
+          }
+  return false;
+  })  
+  */
+  });
+
+}
