d29caa983b3b6fd03186fbcd4683a99e2468f0ae
David Young <david@deepinthecode.com>
2014-06-01 11:19:34 -0500

    Update converter.js

--- js/converter.js
+++ js/converter.js
@@ -167,7 +167,7 @@ DataConverter.prototype.convert = function() {
     
     var mapping = [];
     var targets = decodeURIComponent(getUrlVars()["targets"]).split(',');
-    addTextInputs(targets);
+    addTextInputs(targets,headerNames);
     var transformedText = JSON.stringify(applyMap(targets,mapping,JSON.parse(intermediateText)));
     
     if (this.outputDataType == "json") {
