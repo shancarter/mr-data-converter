8d9ed6e12b66ca6b19c5b4f01a82afc6f204d9b9
David Young <david@deepinthecode.com>
2014-05-31 22:50:51 -0500

    Update jsonToCsv.js
    Adding headers

--- js/jsonToCsv.js
+++ js/jsonToCsv.js
@@ -1,9 +1,15 @@
 // JSON to CSV Converter
-// taken from http://stackoverflow.com/questions/8847766/how-to-convert-json-to-csv-format-and-store-in-a-variable
+// adapted from http://stackoverflow.com/questions/8847766/how-to-convert-json-to-csv-format-and-store-in-a-variable
 function ConvertToCSV(objArray) {
     var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
     var str = '';
+    var headers = '';
+    for (var header in array[0]) {
+        if (headers != '') headers += ','
 
+        headers += array[0][header];
+    }
+    str += headers + '\r\n';
     for (var i = 0; i < array.length; i++) {
         var line = '';
         for (var index in array[i]) {
