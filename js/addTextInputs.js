6670082e6d9207c8b57c375f889fc92153f63ba7
David Young <david@deepinthecode.com>
2014-06-01 11:13:55 -0500

    Update addTextInputs.js

--- js/addTextInputs.js
+++ js/addTextInputs.js
@@ -1,41 +1,11 @@
 function addTextInputs(targets){
   $(document).ready(function() {
-  
-  //var MaxInputs       = 8; //maximum input boxes allowed
-  var InputsWrapper   = $("#InputsWrapper"); //Input boxes wrapper ID
-  
-  $(InputsWrapper).html("");
-  for (i = 0; i < targets.length; i++) {
-    $(InputsWrapper).append('Target ' + (i+1).toString() + ':<input style="width: 50%" type="text" id="target_' + i.toString() + '" value="' + targets[i] + '" readonly /><br />Mapping ' + (i+1).toString() + ':<select style="width: 50%" id="mapping_' + i.toString() + '"></select><br /><br />');
-  };
-  return false;
-  
-  /*
-  var AddButton       = $("#AddMoreFileBox"); //Add button ID
-  
-  var x = InputsWrapper.length; //initlal text box count
-  var FieldCount=1; //to keep track of text box added
-  
-  $(AddButton).click(function (e)  //on add input button click
-  {
-          if(x <= MaxInputs) //max input box allowed
-          {
-              FieldCount++; //text box added increment
-              //add input box
-              $(InputsWrapper).append('<div><input type="text" name="mytext[]" id="field_'+ FieldCount +'" value="Text '+ FieldCount +'"/><a href="#" class="removeclass">&times;</a></div>');
-              x++; //text box increment
-          }
-  return false;
+    var InputsWrapper   = $("#InputsWrapper"); //Input boxes wrapper ID
+    
+    $(InputsWrapper).html("");
+    for (i = 0; i < targets.length; i++) {
+      $(InputsWrapper).append('Target ' + (i+1).toString() + ':<input style="width: 50%" type="text" id="target_' + i.toString() + '" value="' + targets[i] + '" readonly /><br />Mapping ' + (i+1).toString() + ':<select style="width: 50%" id="mapping_' + i.toString() + '"></select><br /><br />');
+    };
+    return false;
   });
-  
-  $("body").on("click",".removeclass", function(e){ //user click on remove text
-          if( x > 1 ) {
-                  $(this).parent('div').remove(); //remove text box
-                  x--; //decrement textbox
-          }
-  return false;
-  })  
-  */
-  });
-
 }
