19f1c635e0efe03a9d40a5e4e7446091676ff094
David Young <david@deepinthecode.com>
2014-05-31 23:13:10 -0500

    Update jsonToCsv.js
    added GetHeaders function

--- js/jsonToCsv.js
+++ js/jsonToCsv.js
@@ -1,14 +1,21 @@
+// from http://stackoverflow.com/questions/1876485/get-property-names-in-json-objects
+function GetHeaders(obj) {
+    var cols = new Array();
+    var p = obj.players[0];
+    for (var key in p) {
+        //alert(' name=' + key + ' value=' + p[key]);
+        cols.push(key);
+    }
+    return cols;
+}
+
 // JSON to CSV Converter
 // adapted from http://stackoverflow.com/questions/8847766/how-to-convert-json-to-csv-format-and-store-in-a-variable
 function ConvertToCSV(objArray) {
     var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
     var str = '';
     
-    var headers = '';
-    for (var index in array) {
-        if (headers != '') headers += ','
-        headers += array[index];
-    }
+    var headers = GetHeaders(objArray);
     str += headers + '\r\n';
     
     for (var i = 0; i < array.length; i++) {
@@ -23,3 +30,5 @@ function ConvertToCSV(objArray) {
 
     return str;
 }
+
+
