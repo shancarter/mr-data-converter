782c549b27ae30e26f36f83b05c8d4fd56d66b47
David Young <david@deepinthecode.com>
2014-05-31 22:57:02 -0500

    Update jsonToCsv.js
    removed header functionality

--- js/jsonToCsv.js
+++ js/jsonToCsv.js
@@ -1,15 +1,8 @@
 // JSON to CSV Converter
-// adapted from http://stackoverflow.com/questions/8847766/how-to-convert-json-to-csv-format-and-store-in-a-variable
+// from http://stackoverflow.com/questions/8847766/how-to-convert-json-to-csv-format-and-store-in-a-variable
 function ConvertToCSV(objArray) {
     var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
     var str = '';
-    var headers = '';
-    for (var header in array[0]) {
-        if (headers != '') headers += ','
-
-        headers += array[0][header];
-    }
-    str += headers + '\r\n';
     for (var i = 0; i < array.length; i++) {
         var line = '';
         for (var index in array[i]) {
