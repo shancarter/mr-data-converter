bf651d06ece1e7f1e3667447e1b7d4083b154564
Drew Shefman <dshefman@squaredi.com>
2014-05-31 19:49:26 -0500

    filtering out fields

--- js/applyMap.js
+++ js/applyMap.js
@@ -1,16 +1,26 @@
 function applymap(targets,mapping,input){
-  var rtn= [];
-  for (var l=0; l< input.length; l++){
-    var line = input[l];
-    var mappedLine = JSON.parse(JSON.stringify(line));
+    var rtn= [];
+    var hash = {};
+    var mappedColumn;
     for (var i= 0; i < mapping.length; i++){
-      var map = mapping[i];
-      var column = Object.getOwnPropertyNames(map)[0];
-      var mappedColumn = map[column];
-      var value = line[mappedColumn];
-      mappedLine[column] = value;
+        var map = mapping[i];
+        var column = Object.getOwnPropertyNames(map)[0];
+        mappedColumn = map[column];
+        hash[column] = mappedColumn;
     }
-    rtn[l] = mappedLine;
-  }
-  return rtn;
+
+    for (var l=0; l< input.length; l++){
+        var line = input[l];
+        var mappedLine = {};
+        for (var inputColumn in line)
+        {
+            if (targets.indexOf(inputColumn) >=0){
+                mappedColumn = (hash[inputColumn] !== undefined) ? hash[inputColumn] : inputColumn;
+                mappedLine[inputColumn] = line[mappedColumn];
+            }
+        }
+
+        rtn[l] = mappedLine;
+    }
+    return rtn;
 }
--- tests/mapping-tests.js
+++ tests/mapping-tests.js
@@ -1,10 +1,19 @@
 describe('applymap',function(){
     it('reverses columns correctly',function(){
-  var targets = ['A','B','C'];
-  var mapping = [{'A':'C'}, {'C':'A'}];
-	var input = [{'A':1,'B':2,'C':3}];
-	var output = applymap(targets, mapping,input);
-	var expected = [{'A':3,'B':2,'C':1}];
-	expect(output).toEqual(expected);
-	});
+        var targets = ['A','B','C'];
+        var mapping = [{'A':'C'}, {'C':'A'}];
+        var input = [{'A':1,'B':2,'C':3}];
+        var output = applymap(targets, mapping,input);
+        var expected = [{'A':3,'B':2,'C':1}];
+        expect(output).toEqual(expected);
+    });
+
+    it('ignores non mapped fields',function(){
+        var targets = ['A','C'];
+        var mapping = [{'A':'C'}, {'C':'A'}];
+        var input = [{'A':1,'B':2,'C':3}];
+        var output = applymap(targets, mapping,input);
+        var expected = [{'A':3,'C':1}];
+        expect(output).toEqual(expected);
+    });
 });
