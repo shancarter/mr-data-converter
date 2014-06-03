fbce040db8cca18439fe392a598505324b17a8e7
David Young <david@deepinthecode.com>
2014-06-03 14:24:36 -0500

    Update addTextInputs.js

--- js/addTextInputs.js
+++ js/addTextInputs.js
@@ -30,9 +30,9 @@ function addTextInputs(targets,headerNames){
   var mapping = [];
   for (k = 0; k < targets.length; k++) {
     if ($("select[id='mapping_" + k.toString() + "']").length != 0 & $("select[id='mapping_" + k.toString() + "'] option:selected").index() != 0) {
-      var val = $("select[id='mapping_" + k.toString() + "']").val();
+      var name = $("select[id='mapping_" + k.toString() + "']").val();
+      var val = $("input[id='target_" + k.toString() + "']").val();
       //alert('val='+val);
-      var name = $("input[id='target_" + k.toString() + "']").val();
       //alert('map='+name);
       var obj = {};
       obj[name] = val;
