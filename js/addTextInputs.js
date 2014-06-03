c0f5496f5b5368163de9cd01d06decbd1d1a75d7
David Young <david@deepinthecode.com>
2014-06-03 01:02:07 -0500

    Update addTextInputs.js
    added value to options

--- js/addTextInputs.js
+++ js/addTextInputs.js
@@ -3,9 +3,9 @@ function addTextInputs(targets,headerNames){
     var InputsWrapper   = $("#InputsWrapper"); //Input boxes wrapper ID
     
     $(InputsWrapper).html("");
-    var headerOptions = "<option></option>\r\n";
+    var headerOptions = "<option value=''>-- No mapping --</option>\r\n";
     for (j = 0; j < headerNames.length; j++) {
-        headerOptions += "  <option>" + headerNames[j].toString() + "</option>\r\n";
+        headerOptions += "  <option value='" + headerNames[j].toString().replace(/</g, '&lt;').replace(/>/g, '&gt;')  + "'>" + headerNames[j].toString()replace(/</g, '&lt;').replace(/>/g, '&gt;') + "</option>\r\n";
     }
     for (i = 0; i < targets.length; i++) {
       var strInputs = '<p><label>Target ' + (i+1).toString() + ':</label><input style="width: 50%" type="text" id="target_' + i.toString() + '" value="' + targets[i] + '" readonly /></p>' 
