c41a55f01fa3d03fe6da857d6381fd5bb64605cf
David Young <david@deepinthecode.com>
2014-06-03 13:18:33 -0500

    Update addTextInputs.js

--- js/addTextInputs.js
+++ js/addTextInputs.js
@@ -31,9 +31,9 @@ function addTextInputs(targets,headerNames){
   for (k = 0; k < targets.length; k++) {
     if ($("select[id='mapping_" + k.toString() + "']").length != 0 & $("select[id='mapping_" + k.toString() + "'] option:selected").index() != 0) {
       var val = $("select[id='mapping_" + k.toString() + "']").val();
-      alert('val='+val);
+      //alert('val='+val);
       var name = $("input[id='target_" + k.toString() + "']").val();
-      alert('map='+name);
+      //alert('map='+name);
       mapping[name] = val;
     }
   }
