fba100fa03b15071a59fa0cce3c747f6d6694d51
David Young <david@deepinthecode.com>
2014-06-03 01:13:07 -0500

    Update addTextInputs.js
    adding

--- js/addTextInputs.js
+++ js/addTextInputs.js
@@ -5,7 +5,7 @@ function addTextInputs(targets,headerNames){
     $(InputsWrapper).html("");
     var headerOptions = "<option value=''>-- No mapping --</option>\r\n";
     for (j = 0; j < headerNames.length; j++) {
-        headerOptions += "  <option value='" + headerNames[j].toString().replace(/</g, '&lt;').replace(/>/g, '&gt;')  + "'>" + headerNames[j].toString()replace(/</g, '&lt;').replace(/>/g, '&gt;') + "</option>\r\n";
+        headerOptions += "  <option value='" + headerNames[j].toString().replace(/</g, '&lt;').replace(/>/g, '&gt;')  + "'>" + headerNames[j].toString().replace(/</g, '&lt;').replace(/>/g, '&gt;') + "</option>\r\n";
     }
     for (i = 0; i < targets.length; i++) {
       var strInputs = '<p><label>Target ' + (i+1).toString() + ':</label><input style="width: 50%" type="text" id="target_' + i.toString() + '" value="' + targets[i] + '" readonly /></p>' 
