fd985b0d7c123ccb9057fc1afcd99bb1767078df
David Young <david@deepinthecode.com>
2014-06-11 01:47:07 -0500

    Added conditional checking if there are no targets

--- js/addTextInputs.js
+++ js/addTextInputs.js
@@ -1,31 +1,33 @@
 function addTextInputs(targets,headerNames){
-  $(document).ready(function() {
-    var InputsWrapper   = $("#InputsWrapper"); //Input boxes wrapper ID
-    var boolSelected = false;
-    for (k = 0; k < targets.length; k++) {
-      if ($("select[id='mapping_" + k.toString() + "']").length != 0 & $("select[id='mapping_" + k.toString() + "'] option:selected").index() != 0) {
-        boolSelected = true;
+  if (targets.length > 0) {
+    $(document).ready(function() {
+      var InputsWrapper   = $("#InputsWrapper"); //Input boxes wrapper ID
+      var boolSelected = false;
+      for (k = 0; k < targets.length; k++) {
+        if ($("select[id='mapping_" + k.toString() + "']").length != 0 & $("select[id='mapping_" + k.toString() + "'] option:selected").index() != 0) {
+          boolSelected = true;
+        };
       };
-    };
-    
-    if (boolSelected == false) {
-      $(InputsWrapper).html("");
-      var headerOptions = "<option value=''>-- none --</option>\r\n";
-      for (j = 0; j < headerNames.length; j++) {
-          headerOptions += "  <option value='" + headerNames[j].toString().replace(/</g, '&lt;').replace(/>/g, '&gt;')  + "'>" + headerNames[j].toString().replace(/</g, '&lt;').replace(/>/g, '&gt;') + "</option>\r\n";
-      }
-      var strInputs = "<table><tr><td>Targets</td><td>Mapping</td></tr>";
-      for (i = 0; i < targets.length; i++) {
-        strInputs += '<tr><td><input style="width:70%" type="text" id="target_' + i.toString() + '" value="' + targets[i].toString().replace(/</g, '&lt;').replace(/>/g, '&gt;') + '" readonly /></td>' 
-          + '<td><select style="width:95%" id="mapping_' + i.toString() + '">'
-          + headerOptions
-          + '</select></td></tr>';
+      
+      if (boolSelected == false) {
+        $(InputsWrapper).html("");
+        var headerOptions = "<option value=''>-- none --</option>\r\n";
+        for (j = 0; j < headerNames.length; j++) {
+            headerOptions += "  <option value='" + headerNames[j].toString().replace(/</g, '&lt;').replace(/>/g, '&gt;')  + "'>" + headerNames[j].toString().replace(/</g, '&lt;').replace(/>/g, '&gt;') + "</option>\r\n";
+        }
+        var strInputs = "<table><tr><td>Targets</td><td>Mapping</td></tr>";
+        for (i = 0; i < targets.length; i++) {
+          strInputs += '<tr><td><input style="width:70%" type="text" id="target_' + i.toString() + '" value="' + targets[i].toString().replace(/</g, '&lt;').replace(/>/g, '&gt;') + '" readonly /></td>' 
+            + '<td><select style="width:95%" id="mapping_' + i.toString() + '">'
+            + headerOptions
+            + '</select></td></tr>';
+        };
+        strInputs += '</table>';
+        $(InputsWrapper).append(strInputs);
       };
-      strInputs += '</table>';
-      $(InputsWrapper).append(strInputs);
-    };
-    return false;
-  });
+      return false;
+    });
+  };
   
   var mapping = [];
   for (k = 0; k < targets.length; k++) {
