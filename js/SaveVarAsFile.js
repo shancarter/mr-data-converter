7ae15523f5c41492a93905692b05315a87885f43
David Young <david@deepinthecode.com>
2014-06-12 17:26:26 -0500

    Code to check for IE, removing extension param

--- js/SaveVarAsFile.js
+++ js/SaveVarAsFile.js
@@ -1,25 +1,15 @@
-//adapted from function on http://www.htmlforums.com/archive/index.php/t-10688.html
-function SaveVarAsFile(someVar,extension){
-    function dataUrl(data) {return "data:x-application/text," + escape(data);}
-    window.open(dataUrl(someVar));
-
-  /*$(document).ready(function() {
-    setTimeout(function() {
-      var ifrm = document.getElementById("SaveFrame");
-      //ifrm = (ifrm.contentWindow) ? ifrm.contentWindow : (ifrm.contentDocument.document) ? ifrm.contentDocument.document : ifrm.contentDocument;
-      ifrm.contentWindow.document.open();
-      ifrm.contentWindow.document.write(someVar);
-      //ifrm.contentWindow.document.focus();
-      //ifrm.contentWindow.document.close();
-      ifrm.contentWindow.document.execCommand('SaveAs',true,'MrCSVTransformer-output.txt');
-      ifrm.contentWindow.document.close();
-    });
-   */ 
-    /*var SaveFrame = frames['SaveFrame'].document;
-    SaveFrame.open("text/html","replace");
-    SaveFrame.write(someVar);
-    SaveFrame.close();
-    SaveFrame.contentWindow.focus();
-    SaveFrame.contentWindow.document.execCommand('SaveAs',true,'MrCSVTransformer-output.'+extension);
-  });*/
+//adapted from function on http://lalp.googlecode.com/svn-history/r29/trunk/WebContent/index.jsp
+function SaveVarAsFile(someVar){
+	function dataUrl(data) {
+	    return "data:x-application/text," + escape(data.replace(/\r?\n/g,"\r\n"));
+	}
+	if("\v"=="v"){
+    	var d= document.open("text/html");
+    	d.write(s);
+    	d.execCommand( "saveAs", true, location.href.split("/").reverse()[0]);
+    	d.close();
+	    return;
+	}
+	window.open(dataUrl(someVar));
+	}
 }
