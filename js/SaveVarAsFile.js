fed8aee0de2c1f71e82ef40404f6e9a529fd3992
David Young <david@deepinthecode.com>
2014-06-12 17:28:58 -0500

    Removing extra brace

--- js/SaveVarAsFile.js
+++ js/SaveVarAsFile.js
@@ -4,12 +4,11 @@ function SaveVarAsFile(someVar){
 	    return "data:x-application/text," + escape(data.replace(/\r?\n/g,"\r\n"));
 	}
 	if("\v"=="v"){
-    	var d= document.open("text/html");
-    	d.write(s);
-    	d.execCommand( "saveAs", true, location.href.split("/").reverse()[0]);
-    	d.close();
+	    var d= document.open("text/html");
+	    d.write(s);
+	    d.execCommand( "saveAs", true, location.href.split("/").reverse()[0]);
+	    d.close();
 	    return;
 	}
 	window.open(dataUrl(someVar));
-	}
 }
