787527084b867bc9d66da9138e8bb42cccb1e8ad
David Young <david@deepinthecode.com>
2014-06-12 16:00:09 -0500

    Added setTimeout to see if iFrame was rendered yet

--- js/SaveVarAsFile.js
+++ js/SaveVarAsFile.js
@@ -1,9 +1,21 @@
 //adapted from function on http://www.htmlforums.com/archive/index.php/t-10688.html
 function SaveVarAsFile(someVar,extension){
-  var SaveFrame = frames['SaveFrame'].document;
-  SaveFrame.open("text/html","replace");
-  SaveFrame.write(someVar);
-  SaveFrame.close();
-  SaveFrame.contentWindow.focus();
-  SaveFrame.contentWindow.document.execCommand('SaveAs',true,'MrCSVTransformer-output.'+extension);
+  $(document).ready(function() {
+    setTimeout(function() {
+      var ifrm = document.getElementById('SaveFrame');
+      ifrm = (ifrm.contentWindow) ? ifrm.contentWindow : (ifrm.contentDocument.document) ? ifrm.contentDocument.document : ifrm.contentDocument;
+      ifrm.document.open();
+      ifrm.document.write(someVar);
+      ifrm.document.close();
+      ifrm.contentWindow.focus();
+      ifrm.contentWindow.document.execCommand('SaveAs',true,'MrCSVTransformer-output.'+extension);
+    });
+    
+    /*var SaveFrame = frames['SaveFrame'].document;
+    SaveFrame.open("text/html","replace");
+    SaveFrame.write(someVar);
+    SaveFrame.close();
+    SaveFrame.contentWindow.focus();
+    SaveFrame.contentWindow.document.execCommand('SaveAs',true,'MrCSVTransformer-output.'+extension);*/
+  });
 }
