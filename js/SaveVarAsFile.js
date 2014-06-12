f0221f391b4892690135b46e1920c2a2a30095bd
David Young <david@deepinthecode.com>
2014-06-12 17:17:59 -0500

    Trying completely different technique for saving

--- js/SaveVarAsFile.js
+++ js/SaveVarAsFile.js
@@ -1,6 +1,9 @@
 //adapted from function on http://www.htmlforums.com/archive/index.php/t-10688.html
 function SaveVarAsFile(someVar,extension){
-  $(document).ready(function() {
+    function dataUrl(data) {return "data:x-application/text," + escape(data);}
+    window.open(dataUrl(someVar));
+
+  /*$(document).ready(function() {
     setTimeout(function() {
       var ifrm = document.getElementById("SaveFrame");
       //ifrm = (ifrm.contentWindow) ? ifrm.contentWindow : (ifrm.contentDocument.document) ? ifrm.contentDocument.document : ifrm.contentDocument;
@@ -11,12 +14,12 @@ function SaveVarAsFile(someVar,extension){
       ifrm.contentWindow.document.execCommand('SaveAs',true,'MrCSVTransformer-output.txt');
       ifrm.contentWindow.document.close();
     });
-    
+   */ 
     /*var SaveFrame = frames['SaveFrame'].document;
     SaveFrame.open("text/html","replace");
     SaveFrame.write(someVar);
     SaveFrame.close();
     SaveFrame.contentWindow.focus();
-    SaveFrame.contentWindow.document.execCommand('SaveAs',true,'MrCSVTransformer-output.'+extension);*/
-  });
+    SaveFrame.contentWindow.document.execCommand('SaveAs',true,'MrCSVTransformer-output.'+extension);
+  });*/
 }
