c669722b401ccc072b0774bda7d12fc49cd21552
David Young <david@deepinthecode.com>
2014-06-12 16:56:44 -0500

    Checking to see if extension is the problem

--- js/SaveVarAsFile.js
+++ js/SaveVarAsFile.js
@@ -2,13 +2,13 @@
 function SaveVarAsFile(someVar,extension){
   $(document).ready(function() {
     setTimeout(function() {
-      var ifrm = document.getElementById('SaveFrame');
-      ifrm = (ifrm.contentWindow) ? ifrm.contentWindow : (ifrm.contentDocument.document) ? ifrm.contentDocument.document : ifrm.contentDocument;
-      ifrm.document.open();
-      ifrm.document.write(someVar);
-      ifrm.document.close();
-      ifrm.focus();
-      ifrm.document.execCommand('SaveAs',true,'MrCSVTransformer-output.'+extension);
+      var ifrm = document.getElementById("SaveFrame");
+      //ifrm = (ifrm.contentWindow) ? ifrm.contentWindow : (ifrm.contentDocument.document) ? ifrm.contentDocument.document : ifrm.contentDocument;
+      ifrm.contentWindow.document.open();
+      ifrm.contentWindow.document.write(someVar);
+      ifrm.contentWindow.document.focus();
+      ifrm.contentWindow.document.close();
+      ifrm.contentWindow.document.execCommand('SaveAs',true,'MrCSVTransformer-output.txt');
     });
     
     /*var SaveFrame = frames['SaveFrame'].document;
