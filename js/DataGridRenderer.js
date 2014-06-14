f135641cbdc887ef1ca4ba4263ee07ee3e2572b1
David Young <david@deepinthecode.com>
2014-06-14 00:35:34 -0500

    Adding mongoDB insert format

--- js/DataGridRenderer.js
+++ js/DataGridRenderer.js
@@ -228,6 +228,43 @@ var DataGridRenderer = {
     return outputText;
   },
   
+  //---------------------------------------
+  // mongoDB
+  //---------------------------------------
+  
+  mongodb: function (dataGrid, headerNames, headerTypes, indent, newLine) {
+    //inits...
+    var commentLine = "//";
+    var commentLineEnd = "";
+    var outputText = "";
+    var numRows = dataGrid.length;
+    var numColumns = headerNames.length;
+    
+    outputText = "db.collection.insert("+newLine;
+    outputText += indent+"["+newLine;
+    //begin render loop
+    for (var i=0; i < numRows; i++) {
+      var row = dataGrid[i];
+      outputText += indent+indent+"{";
+      for (var j=0; j < numColumns; j++) {
+        if ((headerTypes[j] == "int")||(headerTypes[j] == "float")) {
+          var rowOutput = row[j] || "null";
+        } else {
+          var rowOutput = '"' + ( row[j] || "" ) + '"';
+        };
+  
+      outputText += ('"'+headerNames[j] +'"' + ":" + rowOutput );
+  
+        if (j < (numColumns-1)) {outputText+=","};
+      };
+      outputText += "}";
+      if (i < (numRows-1)) {outputText += ","+newLine};
+    };
+    outputText += newLine+indent+"]"+newLine;
+    outputText += ")";
+    
+    return outputText;
+  },
   
   //---------------------------------------
   // MYSQL
