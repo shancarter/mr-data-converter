75055f875e8970235ce39e1bea2ae766b985e70a
David Young <david@deepinthecode.com>
2014-06-15 08:42:17 -0500

    Create enableTab.js

---
+++ js/enableTab.js
@@ -0,0 +1,29 @@
+// adapted from http://jsfiddle.net/2wAzx/13/
+
+function enableTab(id) {
+    var el = document.getElementById(id);
+    el.onkeydown = function(e) {
+        if (e.keyCode === 9) { // tab was pressed
+
+            // get caret position/selection
+            var val = this.value,
+                start = this.selectionStart,
+                end = this.selectionEnd;
+
+            // set textarea value to: text before caret + tab + text after caret
+            this.value = val.substring(0, start) + '\t' + val.substring(end);
+
+            // put caret at right position again
+            this.selectionStart = this.selectionEnd = start + 1;
+
+            // prevent the focus lose
+            return false;
+
+        }
+    };
+}
+
+// Enable the tab character onkeypress (onkeydown) inside textarea...
+// ... for a textarea that has an `id="my-textarea"`
+//enableTab('my-textarea');
+enableTab('dataInput');
