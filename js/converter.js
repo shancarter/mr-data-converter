3c8fea3bef7545abe0bb71446ad12a4f6f6ca980
Paul Curry <github@cr3ation.co.uk>
2015-11-27 14:27:07 +0000

    Captain Javascript to the rescue

--- js/converter.js
+++ js/converter.js
@@ -32,7 +32,7 @@ function DataConverter(nodeId) {
                                 {"text":"XML - Properties",       "id":"xmlProperties",    "notes":""},
                                 {"text":"XML - Nodes",            "id":"xml",              "notes":""},
                                 {"text":"XML - Illustrator",      "id":"xmlIllustrator",   "notes":""}];
-  this.outputDataType         = "json";
+  this.outputDataType         = "jsonArrayRowsText";
 
   this.columnDelimiter        = "\t";
   this.rowDelimiter           = "\n";
